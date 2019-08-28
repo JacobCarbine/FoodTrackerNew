3672143a5446dc03aa30f66591dbb41b58136676
Jacob Carbine <jwcarbine2@yahoo.com>
2019-08-28 10:33:24 -0600

    We golden

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Jacob Carbine on 8/26/19.
+//  Copyright © 2019 Jacob Carbine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    //MARK: Properties
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
